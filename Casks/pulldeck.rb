af48f2178bdc311dc3d33e146ae1e3f5bf461f04
Clément Billiot <clement.billiot@gmail.com>
2026-09-10 09:19:58 +0200

    fix: remove deprecated cask URL verified parameter

--- Casks/pulldeck.rb
+++ Casks/pulldeck.rb
@@ -2,8 +2,7 @@ cask "pulldeck" do
   version "0.2.8"
   sha256 "8df482b39f225a0f07bf327f03963ea3b69b5dde3445432105a9d30097282eb8"
 
-  url "https://github.com/lumargo/pulldeck-releases/releases/download/v#{version}/PullDeck.dmg",
-      verified: "github.com/lumargo/pulldeck-releases/"
+  url "https://github.com/lumargo/pulldeck-releases/releases/download/v#{version}/PullDeck.dmg"
   name "PullDeck"
   desc "Menu-bar app that prioritizes your GitHub pull requests"
   homepage "https://pulldeck.dev/"
