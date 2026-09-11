b357958b496dc394ac282d08a7fee8793dc5a4ca
Clément Billiot <clement.billiot@gmail.com>
2026-09-12 00:34:57 +0200

    feat: pulldeck cask 0.2.10

--- Casks/pulldeck.rb
+++ Casks/pulldeck.rb
@@ -1,6 +1,6 @@
 cask "pulldeck" do
-  version "0.2.9"
-  sha256 "c71582ba11bbfc713c6d280927d79d7e480a47a8ed343c37c587e92a6ead1d1a"
+  version "0.2.10"
+  sha256 "fba2bb7fda5a2cec6029348dd52a58e114c8c68aa21ca8831bf4b90efc60f60b"
 
   url "https://github.com/lumargo/pulldeck-releases/releases/download/v#{version}/PullDeck.dmg"
   name "PullDeck"
